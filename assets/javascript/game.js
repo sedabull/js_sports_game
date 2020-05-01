bd5c1d4d49bd7271feff40af30992e44223d4c66
sedabull <sedabull@gmail.com>
2020-05-01 17:40:50 -0400

    earn basic rubric points

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+const reset = document.querySelector('#reset-button');
+const numResets = document.querySelector('#num-resets');
+
+const teamOneShoot = document.querySelector('#teamone-shoot-button');
+const teamOneShots = document.querySelector('#teamone-numshots');
+const teamOneGoals = document.querySelector('#teamone-numgoals');
+
+const teamTwoShoot = document.querySelector('#teamtwo-shoot-button');
+const teamTwoShots = document.querySelector('#teamtwo-numshots');
+const teamTwoGoals = document.querySelector('#teamtwo-numgoals');
+
+reset.addEventListener('click', function() {
+    teamOneShots.innerText = '0';
+    teamOneGoals.innerText = '0';
+    teamTwoShots.innerText = '0';
+    teamTwoGoals.innerText = '0';
+    numResets.innerText = Number(numResets.innerText) + 1;
+});//end reset click
+
+teamOneShoot.addEventListener('click', function() {
+    teamOneShots.innerText = Number(teamOneShots.innerText) + 1;
+    if (Math.random() < .3) {
+        teamOneGoals.innerText = Number(teamOneGoals.innerText) + 1;
+    }//end if
+});//end teamOneShoot click
+
+teamTwoShoot.addEventListener('click', function() {
+    teamTwoShots.innerText = Number(teamTwoShots.innerText) + 1;
+    if (Math.random() < .3) {
+        teamTwoGoals.innerText = Number(teamTwoGoals.innerText) + 1;
+    }//end if
+});//end teamOneShoot click
